f706b53eb523e39889f26f885bb3af5de51b9ad3
Fenhl <fenhl@fenhl.net>
2019-02-13 15:56:01 +0000

    Add accessors for notification's `item` field

--- src/model/notification.rs
+++ src/model/notification.rs
@@ -5,14 +5,37 @@ use std::{
     iter::FromIterator
 };
 use chrono::prelude::*;
+use reqwest::Url;
 use super::super::{
     Result,
     client::{
         AnnotatedData,
         Auth,
         Client
-    }
+    },
+    util::UrlDef
 };
+
+/// The kind of link contained in a notification. Returned by `Notification::webllink_rel`.
+#[derive(Debug, Deserialize, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
+pub enum Rel {
+    /// someone liked the forum post
+    Post,
+    /// a run is linked from the notification
+    Run,
+    /// when a game request was approved/denied
+    Game,
+    /// when a guide was updated
+    Guide
+}
+
+#[derive(Debug, Deserialize, Clone)]
+struct Item {
+    rel: Rel,
+    #[serde(with = "UrlDef")]
+    uri: Url
+}
 
 #[derive(Debug, Deserialize, Clone, Copy)]
 #[serde(rename_all = "snake_case")]
@@ -35,6 +58,7 @@ impl From<ReadStatus> for bool {
 pub struct NotificationData {
     id: String,
     created: DateTime<Utc>,
+    item: Item,
     status: ReadStatus,
     text: String
 }
@@ -61,6 +85,16 @@ impl Notification {
     /// Returns `true` if this notification is marked as read.
     pub fn read(&self) -> bool {
         self.data.status.into()
+    }
+
+    /// Returns the link contained in this notification. May point to the homepage.
+    pub fn weblink(&self) -> &Url {
+        &self.data.item.uri
+    }
+
+    /// The kind of item the `weblink` points at.
+    pub fn weblink_rel(&self) -> Rel {
+        self.data.item.rel
     }
 }
 
